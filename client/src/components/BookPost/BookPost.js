a3e91d6c9176af9dbfef893eb29a9661e92f01c5
elpat7 <aguilarpat7@gmail.com>
2020-03-01 18:05:56 -0800

    adding functionality to get book information

---
+++ client/src/components/BookPost/BookPost.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import axios from 'axios';
+
+
+function BookList(props) {
+    const list = props.
+
+        return(
+            <div>Hello from booklist</div>
+        )
+}
+
+
+
+export default BookList;
--- client/src/components/BookSearch/BookSearch.js
+++ client/src/components/BookSearch/BookSearch.js
@@ -1,41 +1,34 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import './booksearch.css';
+import BookList from '../BookPost/BookPost'
 
-function BookSearch(props) {
-  const [counter, changeCounter] = useState(0);
-
-
-  // component lifecycle - renders to screen (component mounts), 
-  // receives props, build state, component updates, 
-  // lastly the componen unmounts
-  //this is same as component will mount - what happens when the component is loaded on the screen
-  // when about is called, it will print hello
-  // when leaving about, it will print goodbye because we left that comoponent
-  useEffect(() => {
-    console.log("hello, you just came to the book search component");
-    return () => console.log("goodbye, you are leaving about");
-  }, []);
-
-  useEffect(() => {
-    console.log(counter);
-  }, [counter]);
-
-  // function addOne() {
-  //   changeCounter(counter + 1);
-  // }
+function BookSearch() {
+  const [input, setInput] = useState('');
+  let book = []
 
   function getBooks() {
-    let url = `https://www.googleapis.com/books/v1/volumes?q=quilting`
+    let url = `https://www.googleapis.com/books/v1/volumes?q=${input}`
 
     axios.get(url).then(result => {
       const data = result.data.items;
       console.log('data', data)
-      console.log(data[1].volumeInfo.title)
 
       for (let i = 0; i < data.length; i++) {
         let bookTitles = data[i].volumeInfo.title;
-        console.log(bookTitles);
+        let authors = data[i].volumeInfo.authors;
+        let description = data[i].volumeInfo.description;
+        let image = data[i].volumeInfo.imageLinks.thumbnail;
+        let link = data[i].volumeInfo.previewLink;
+        let id = data[i].id;
+        book.push(bookTitles, authors, description, image, link, id)
+        console.log(book);
+
+        // console.log(authors);
+        // console.log(description);
+        // console.log(image);
+        // console.log(link);
+        // console.log(id);
       }
     })
   }
@@ -44,12 +37,18 @@ function BookSearch(props) {
     <div>
       <div className="bookSearch">
         <h1>Type in a book name:</h1>
-        <div><input type="text" id="bookInput"></input></div>
+        <div><input value={input} id="bookInput" onInput={e => setInput(e.target.value)} /></div>
         <br></br>
         <button className="btn btn-lg btn-primary" id="searchBooks" onClick={() => getBooks()}><i class="fa fa-search"></i> Show me the book</button>
       </div>
       <div className="container justify-content-center col-md-10 bookDisplay">
-        <h1> the list of books will display here</h1>
+
+        <div class="card text-center">
+          <div class="card-header">
+            <h1 class="card-title" id="bookDisplay">The books are:</h1>
+          </div>
+          <BookList />
+        </div>
       </div>
     </div>
   )
